cfbd58d799ef8553ba8c7eba07062ef133b77dbc
Erin Pentecost <erin@pentecost.email>
2026-07-25 17:25:31 -0700

    add LOD node support

--- properties/object_properties.py
+++ properties/object_properties.py
@@ -4,6 +4,34 @@ import bpy
 class ObjectProperties(bpy.types.PropertyGroup):
     object_flags: bpy.props.IntProperty(name="Flags", min=0, max=65535, default=2)
 
+    is_lod_node: bpy.props.BoolProperty(
+        name="LOD Node",
+        description=(
+            "Export this object as a NiLODNode."
+            " Children are used as LOD levels, ordered from highest to lowest detail,"
+            " each visible within its own 'Far Extent' distance range"
+        ),
+        default=False,
+    )
+
+    lod_center: bpy.props.FloatVectorProperty(
+        name="LOD Center",
+        description="Center point used for LOD distance calculations, in local space",
+        size=3,
+        default=(0.0, 0.0, 0.0),
+        subtype="XYZ",
+    )
+
+    lod_far_extent: bpy.props.FloatProperty(
+        name="Far Extent",
+        description=(
+            "Maximum distance, in game units, at which this LOD level is visible."
+            " The near distance is set automatically to the previous level's far extent (0 for the first level)"
+        ),
+        default=0.0,
+        min=0.0,
+    )
+
     @staticmethod
     def register() -> None:
         bpy.types.Object.mw = bpy.props.PointerProperty(type=ObjectProperties)
